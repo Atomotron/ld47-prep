e0cc32550dca5487b8abd4fdd850d7923d0d3068
Your Name <you@example.com>
2020-09-24 17:53:12 -0500

    Vector class

--- math32.js
+++ math32.js
@@ -4,6 +4,11 @@
  *     Scalar.Add(out,x,y) // Addition with uncoupled output
  *     y.addeq(x) // In-place addition, changes y, returns this for chaining.
  *     y.add(x)   // Allocates and returns a new scalar
+ *
+ * Note: scalar-valued functions on vector take and return JS numbers which we expect the
+ *       JIT to specially detect and deal with as f32s. This makes the interface less uniform,
+ *       but I believe the convience of being able to write normal arithmetic expressions makes
+ *       up for that.
  */
 class BufferFloats {
     constructor(array) {
@@ -27,9 +32,12 @@ class Scalar extends BufferFloats {
         super(n);
     }
     // Creates a new Scalar equal to this one
-    clone() {
-        return (new Scalar()).eq(this);
-    }
+    clone() {return new Scalar(this.a[0]);}
+    // Returns inner value
+    valueOf() {return this.a[0];}
+    toString() {return this.a[0].toString();}
+    // Assigns a value, taking a JS Number instead of a Scalar like Eq would.
+    set(x) {this.a[0] = x;}
     
     /** Operations (Constant) **/
     static Zero(out) {
@@ -97,7 +105,14 @@ class Scalar extends BufferFloats {
     diveq(x) {return Scalar.Div(this,this,x);}
     div(x) {return this.clone().diveq(x);}
 }
-
+/*
+ * 2D Vector
+ * Supports:
+ * - Addition, Subtraction, Scalar Multiplication (Add, Sub, Mul)
+ * - Length, Distance, Squared length (Abs, Dist, Sq)
+ * - Dot product, Cross product, projection, scalar projection (Dot, Cross, Proj, Resolute) 
+ * - Normalization, magnitude mapping (Norm, MapAbs)
+ */
 class Vec extends BufferFloats {
     /** Constructors **/
     constructor(x=0.0,y=0.0) {
@@ -107,15 +122,13 @@ class Vec extends BufferFloats {
         super(a);
     }
     // Creates a new Vector equal to this one
-    clone() {
-        return (new Vec()).eq(this);
-    }    
+    clone() {return new Vec(this.a[0],this.a[1]);}    
     
     /** Operations (Binary, Vector-Valued) **/
     // Scalar Multiplication
     static Mul(out,x,y_scalar) {
-        out.a[0] = x.a[0]*y_scalar.a[0];
-        out.a[1] = x.a[1]*y_scalar.a[0];
+        out.a[0] = x.a[0]*y_scalar;
+        out.a[1] = x.a[1]*y_scalar;
         return out;
     }
     muleq(x) {return Vec.Mul(this,this,x);}
@@ -139,40 +152,69 @@ class Vec extends BufferFloats {
     subeq(x) {return Vec.Sub(this,this,x);}
     sub(x) {return this.clone().subeq(x);}
     
+    // Projects y onto x
+    static Proj(out,x,y) {
+        let y_mag = Vec.Abs(y);
+        if (y_mag === 0) {
+            out.eq(y);
+        } else {
+            let resolute = Vec.Resolute(x,y);
+            let ratio = Math.fround(resolute / y_mag);
+            out.a[0] = y[0] * ratio;
+            out.a[1] = y[1] * ratio;
+        }
+        return out;
+    }
+    projeq(y) { // A slight break from convention: it's the argument that gets overwritten, because it's what we are projecting.
+        return Vec.Proj(y,this,y);
+    }
+    proj(y) {return this.projeq(y.clone())};
+    
     /** Operations (Binary, Scalar-Valued) **/
     // Dot product
-    static Dot(out_scalar,x,y) {
-        out_scalar.a[0] = Math.fround(x.a[0]*x.a[0]) + Math.fround(y.a[1]*y.a[1]);
-        return out_scalar;
+    static Dot(x,y) {
+        return Math.fround(x.a[0]*x.a[0]) + Math.fround(y.a[1]*y.a[1]);
     }
-    dot(x) {return Vec.Dot(new Scalar(),this,x);}
+    dot(x) {return Vec.Dot(this,x);}
     
     // Cross product (Actually a pseudo cross-product because the real one is in 3D)
-    static Cross(out_scalar,x,y) {
-        out_scalar.a[0] = Math.fround(x.a[0]*y.a[1]) - Math.fround(x.a[1]*y.a[0]);
-        return out_scalar;
+    static Cross(x,y) {
+        return Math.fround(x.a[0]*y.a[1]) - Math.fround(x.a[1]*y.a[0]);
     }
-    cross(x) {return Vec.Cross(new Scalar(),this,x);}
+    cross(x) {return Vec.Cross(this,x);}
+    
+    // Distance between vectors
+    static Dist(x,y) {
+        let dx = Math.fround(y.a[0]-x.a[0]);
+        let dy = Math.fround(y.a[1]-x.a[1]);
+        return Math.fround(Math.sqrt(Math.fround(Math.fround(dx*dx)+Math.fround(dy*dy))));
+    }
+    dist(x) {return Vec.Dist(this,x)};
+    
+    // Scalar projection, also called "scalar resolute." Projects y on to x.
+    static Resolute(x,y) {
+        let x_mag = Vec.Abs(x);
+        let dot = Vec.Dot(x,y);
+        return Math.fround(dot / x_mag);
+    }
     
     /** Operations (Unary, Scalar-Valued) **/
     // Magnitude squared (scalar output, sqeq therefore does not exist)
-    static Sq(out_scalar,x) {
-        Vec.Dot(out_scalar,x,x);
-        return out_scalar;
+    static Sq(x) {
+        return Vec.Dot(x,x);
     }
-    sq() {return Vec.Sq((new Scalar()),this);}
+    sq() {return Vec.Sq(this);}
     
     // Vector length (sqrt of mag squared) (scalar output, abseq therefore does not exist)
-    static Abs(out_scalar,x) {
-        Vec.Sq(out_scalar,x);
-        out_scalar.sqrteq();
+    static Abs(x) {
+        return Math.fround(Math.sqrt(Vec.Sq(x)));
     }
-    abs() {return Vec.Abs(new Scalar(),this);}
+    abs() {return Vec.Abs(this);}
     
     /** Operations (Unary, Vector-Valued) **/
     // Normalization: makes a vector unit-length but keeps its direction
     static Norm(out,x) {
-        let mag = Math.sqrt(Math.fround(x.a[0]*x.a[0]) + Math.fround(x.a[1]*x.a[1]));
+        let mag = Vec.Abs(x);
         out.eq(x);
         out.diveq(mag);
         return out;
@@ -197,6 +239,4 @@ class Vec extends BufferFloats {
     }
     mapabseq(f) {return Vec.MapAbs(this,this,f);}
     mapabs(f) {return this.clone().mapabseq(f);}
-    
-
 }
